916d86d813f6ed594d486e5475593f5ee6fb7a7c
xqf <xqf@eeepay.cn>
2016-03-08 10:24:01 +0800

    test

--- app/src/main/java/eeepay/androidmorefunctiondemo/Test.java
+++ app/src/main/java/eeepay/androidmorefunctiondemo/Test.java
@@ -42,6 +42,10 @@ public class Test extends AppCompatActivity {
 //test
      Intent intent=new Intent(this, CameraWeviewAct.class);
          startActivity(intent);
+        //test
+        //test
+        //test
+        //test
 
     }
 
