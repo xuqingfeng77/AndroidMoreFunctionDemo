1ad364dbd572056375cf4bb98797d957a476b65d
xqf <xqf@eeepay.cn>
2016-03-11 19:10:51 +0800

    添加微信分享功能

--- app/src/main/java/eeepay/androidmorefunctiondemo/Test.java
+++ app/src/main/java/eeepay/androidmorefunctiondemo/Test.java
@@ -1,6 +1,8 @@
 package eeepay.androidmorefunctiondemo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.PersistableBundle;
@@ -15,6 +17,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.sdk.modelmsg.WXTextObject;
+import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -23,13 +35,15 @@ import java.io.ObjectOutputStream;
 
 import eeepay.androidmorefunctiondemo.html.CameraWeviewAct;
 import eeepay.androidmorefunctiondemo.util.CacheUtil;
+import eeepay.androidmorefunctiondemo.util.Util;
 
-public class Test extends AppCompatActivity {
+public class Test extends AppCompatActivity  implements IWXAPIEventHandler {
     private static final int DECIMAL_DIGITS = 1;
     EditText mEtxt;
     Button save;
     Button get;
-
+    public  static String ID="wx3158193417ea54ca";
+    private IWXAPI api;
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
@@ -39,9 +53,11 @@ public class Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        api = WXAPIFactory.createWXAPI(this, ID, false);
+        api.registerApp(ID);
 //test
-     Intent intent=new Intent(this, CameraWeviewAct.class);
-         startActivity(intent);
+//     Intent intent=new Intent(this, CameraWeviewAct.class);
+//         startActivity(intent);
         //test
         //test
         //test
@@ -51,5 +67,53 @@ public class Test extends AppCompatActivity {
         //test
 
     }
+    public  void ShareClick(View v){
+        // 初始化一个WXTextObject对象
+        WXTextObject textObj = new WXTextObject();
+        textObj.text = "111111111111111111111111";
 
+        // 用WXTextObject对象初始化一个WXMediaMessage对象
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = textObj;
+        // 发送文本类型的消息时，title字段不起作用
+        // msg.title = "Will be ignored";
+        msg.description = "222222222222222222";
+
+        // 构造一个Req
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("text"); // transaction字段用于唯一标识一个请求
+        req.message = msg;
+        req.scene = false ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+
+        // 调用api接口发送数据到微信
+        api.sendReq(req);
+    }
+    public void ShareUrlClick(View v){
+        WXWebpageObject webpage = new WXWebpageObject();
+        webpage.webpageUrl = "http://www.baidu.com";
+        WXMediaMessage msg = new WXMediaMessage(webpage);
+        msg.title = "WebPage Title WebPage Title WebPage Title WebPage Title WebPage Title WebPage Title WebPage Title WebPage Title WebPage Title Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+        msg.description = "WebPage Description WebPage Description WebPage Description WebPage Description WebPage Description WebPage Description WebPage Description WebPage Description WebPage Description Very Long Very Long Very Long Very Long Very Long Very Long Very Long";
+        Bitmap thumb = BitmapFactory.decodeResource(getResources(), R.mipmap.icon_data_select);
+        msg.thumbData = Util.bmpToByteArray(thumb, true);
+
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("webpage");
+        req.message = msg;
+        req.scene = false ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+        api.sendReq(req);
+
+    }
+    private String buildTransaction(final String type) {
+        return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
+    }
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+
+    }
 }
