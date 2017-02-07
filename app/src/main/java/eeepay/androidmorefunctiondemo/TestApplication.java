e203155f600a9a44f4a65d1b0abf0429c23ae26d
xqf <xqf@eeepay.cn>
2017-02-07 14:12:04 +0800

    add file

--- app/src/main/java/eeepay/androidmorefunctiondemo/TestApplication.java
+++ app/src/main/java/eeepay/androidmorefunctiondemo/TestApplication.java
@@ -11,6 +11,12 @@ public class TestApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         LeakCanary.install(this);
+        // Normal app init code...
     }
 }
